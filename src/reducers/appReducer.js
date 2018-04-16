9e798f54c35f9d0dbb564c2e436bdf7c1be43203
grabeqx <grabowski.kontakt@gmail.com>
2018-04-16 09:54:54 +0200

    Init

---
+++ src/reducers/appReducer.js
@@ -0,0 +1,11 @@
+const appReducer = (state = {
+
+}, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
+
+export default appReducer;
---
+++ src/sagas/sagas.js
@@ -0,0 +1,10 @@
+import {select, call, put, takeLatest, all } from 'redux-saga/effects';
+
+import ACTIONS from '../constants/actions';
+
+
+function* sejmikSaga() {
+
+}
+
+export default sejmikSaga;
